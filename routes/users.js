75b471f080b0fcfe34b2fc0315dd85ffcc4cc971
op8 <jrc300@gmail.com>
2016-02-11 02:46:47 -0800

    updated user dashboard and added initial vote form

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('dashboard', { title: 'Dashboard' });
 });
 
+router.post('/vote', function(req, res) {
+  console.log(req.body.candidate);
+  //res.send(req.body.optradio);
+  //res.send(200);
+});
+
+
 module.exports = router;
