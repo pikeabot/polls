18b1611be5daa936aec784948e36069da8cda9a5
op8 <jrc300@gmail.com>
2016-02-12 03:06:51 -0800

    Added mongodb functionality to poll

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://172.17.0.1:27017/polls');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var demSchema = mongoose.Schema({
+    Bernie: Number,
+    Hillary: Number
+
+});
+
+var DemVote = mongoose.model('DemVote', demSchema);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,9 +23,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/vote', function(req, res) {
-  console.log(req.body.candidate);
-  //res.send(req.body.optradio);
-  //res.send(200);
+	DemVote.findOne({ }, function (err, demvotes){
+		if (req.body.candidate == 'Bernie') {
+  			demvotes.Bernie++;
+  		}
+  		else {
+  			demvotes.Hillary++;
+  		}
+  		demvotes.save();
+	});
 });
 
 
