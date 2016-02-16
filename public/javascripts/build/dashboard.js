7dae5d297267b86abe55e901b186ef81672aa8d5
op8 <jrc300@gmail.com>
2016-02-16 13:21:44 -0800

    react code to change poll

---
+++ public/javascripts/build/dashboard.js
@@ -0,0 +1,60 @@
+var changeToRepPollLink = React.createClass({
+  getInitialState: function() {
+    return {showRepPoll: false};
+  },
+	onClick : function() {
+       this.setState({ showRepPoll : true} );
+    },
+  render: function() {
+  	if(this.state.showRepPoll) { 
+	    return (
+        React.render(
+          React.createElement("div", {id: "rep-poll"}, 
+          React.createElement("h3", null, "Who do you think will get the Republican nomination?"), 
+            React.createElement("form", {action: "/users/vote/Rep", method: "post"}, 
+              React.createElement("div", {class: "radio"}, 
+                React.createElement("input", {type: "radio", name: "candidate", id: "optionsTrump", value: "Trump"}), 
+                  React.createElement("label", null, "Trump")
+              ), 
+              React.createElement("div", {class: "radio"}, 
+                React.createElement("input", {type: "radio", name: "candidate", id: "optionsCruz", value: "Cruz"}), 
+                  React.createElement("label", null, "Cruz")
+              )
+            )
+          ),
+          document.getElementById('poll')
+        )
+        )
+		}
+		else { 
+      return (
+      	React.createElement("a", {onClick: this.onClick}, " Republican ")
+      	);
+     } 
+  	}
+	});
+
+
+
+var RepPollLink = React.createClass({
+  render: function() {
+    React.createElement("div", {id: "rep-poll"}, 
+    React.createElement("h3", null, "Who do you think will get the Republican nomination?"), 
+      React.createElement("form", {action: "/users/vote/Rep", method: "post"}, 
+        React.createElement("div", {class: "radio"}, 
+          React.createElement("input", {type: "radio", name: "candidate", id: "optionsTrump", value: "Trump"}), 
+            React.createElement("label", null, "Trump")
+        ), 
+        React.createElement("div", {class: "radio"}, 
+          React.createElement("input", {type: "radio", name: "candidate", id: "optionsCruz", value: "Cruz"}), 
+            React.createElement("label", null, "Cruz")
+        )
+      )
+    )
+  }
+});
+
+ReactDOM.render(
+  React.createElement(changeToRepPollLink, null),
+  document.getElementById('rep-poll-link')
+);
